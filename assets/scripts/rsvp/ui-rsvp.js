48d23a9973e3c03d8d656ebdcca106462195f492
drewmccarron <drewcmccarron@gmail.com>
2020-03-17 00:50:11 -0400

    Fixed bug with overlapping text
    Updating the font size of the main message. Additionally, I updated
    some of the UI messages to be shorter.

--- assets/scripts/rsvp/ui-rsvp.js
+++ assets/scripts/rsvp/ui-rsvp.js
@@ -11,7 +11,7 @@ const onEventCreateSuccess = function (data) {
 }
 
 const onEventCreateFailure = function (data) {
-  $('#user-messages').html('Failed to create event, please try again')
+  $('#user-messages').html('Failed to create event')
 }
 
 const onEventUpdateSuccess = function (data) {
@@ -21,15 +21,15 @@ const onEventUpdateSuccess = function (data) {
 }
 
 const onEventUpdateFailure = function (data) {
-  $('#user-messages').html('Failed to update event, please try again')
+  $('#user-messages').html('Failed to update event')
 }
 
 const onEventDeleteSuccess = function (data) {
-  $('#user-messages').html('Event was deleted')
+  $('#user-messages').html('Event deleted')
 }
 
 const onEventDeleteFailure = function (data) {
-  $('#user-messages').html('Failed to delete event, please try again')
+  $('#user-messages').html('Failed to delete event')
 }
 
 const onEventIndexSuccess = function (data) {
@@ -63,35 +63,35 @@ const onUpdateHandlebarsFailure = function (data) {
 }
 
 const onRsvpEventSuccess = function (data) {
-  $('#user-messages').html('Successfully RSVP to event')
+  $('#user-messages').html("Successfully RSVP'd")
 }
 
 const onRsvpEventFailure = function (data) {
-  $('#user-messages').html('Failed to RSVP, please try again')
+  $('#user-messages').html('Failed to RSVP')
 }
 
 const onUnRsvpEventSuccess = function (data) {
-  $('#user-messages').html('Successfully Un-RSVP to event')
+  $('#user-messages').html("Successfully qUn-RSVP'd")
 }
 
 const onUnRsvpEventFailure = function (data) {
-  $('#user-messages').html('Failed to Un-RSVP to event, please try again')
+  $('#user-messages').html('Failed to Un-RSVP')
 }
 
 const onRsvpUserSuccess = function (data) {
-  $('#user-messages').html('Successfully RSVP to event')
+  $('#user-messages').html("Successfully RSVP'd")
 }
 
 const onRsvpUserFailure = function (data) {
-  $('#user-messages').html('Failed to Un-RSVP to event, please try again')
+  $('#user-messages').html('Failed to Un-RSVP')
 }
 
 const onUnRsvpUserSuccess = function (data) {
-  $('#user-messages').html('Successfully Un-RSVP to event')
+  $('#user-messages').html('Successfully Un-RSVP')
 }
 
 const onUnRsvpUserFailure = function (data) {
-  $('#user-messages').html('Failed to Un-RSVP to event, please try again')
+  $('#user-messages').html('Failed to Un-RSVP')
 }
 
 const showOwnedSuccess = function (data) {
@@ -102,7 +102,7 @@ const showOwnedSuccess = function (data) {
 }
 
 const showOwnedFailure = function (data) {
-  $('#user-messages').html('Showing Your events')
+  $('#user-messages').html('Failed to get your events')
 }
 
 const showUnsignedSuccess = function (data) {
